b65d94f8e9bae437b1054c9c53c0fa2deb2a076b
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-06-20 23:50:01 +0300

    add detailsPage

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import { Layout } from './Layout';
 import TodosPage from 'pages/TodosPage';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from 'pages/HomePage';
+import TodosDetailsPage from 'pages/TodosDetailsPage';
 
 export const App = () => {
   // const dispatch = useDispatch();
@@ -15,6 +16,7 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="todos" element={<TodosPage />} />
+        <Route path="todos/:todoId" element={<TodosDetailsPage />} />
         <Route path="register" element={<RegisterPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="*" element={<HomePage />} />
